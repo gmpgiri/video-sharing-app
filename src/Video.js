4cb7950661767e50a1bd687600d33514ef8cbc52
Giri Madhan <gmpgiri@gmail.com>
2020-09-01 22:08:28 +0530

    first commit

---
+++ src/Video.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Video.css";
+
+const Video = () => {
+  return (
+    <div className="video">
+      <video src="https://youtu.be/yL8KL4KlvuQ"></video>
+      {/* <VideoFooter /> */}
+      {/* <VideoSidebar /> */}
+    </div>
+  );
+};
+
+export default Video;
